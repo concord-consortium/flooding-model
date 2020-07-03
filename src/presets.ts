fbe6745e3e21092aaa530b1ea135ab25a4534764
pjanik <janikpiotrek@gmail.com>
2020-07-03 14:00:30 +0200

    Change elevationVerticalTitl to value in %
    [#173623189]
    
    So it doesn't depend on heightmapMaxElevation.

--- src/presets.ts
+++ src/presets.ts
@@ -1,7 +1,7 @@
 import { ISimulationConfig } from "./config";
 
 const presets: {[key: string]: Partial<ISimulationConfig>} = {
-  iowa: {
+  Iowa: {
     elevation: "data/iowa-city-heightmap.png",
     riverData: "data/iowa-city-river.png",
     texture: "data/iowa-city-streetmap.png",
@@ -10,41 +10,50 @@ const presets: {[key: string]: Partial<ISimulationConfig>} = {
     modelHeight: 13000, // around 13km, measured on google maps
     modelWidth: 20000
   },
-  iowaTilt: {
+  IowaTilt: {
     elevation: "data/iowa-city-heightmap.png",
     riverData: "data/iowa-city-river.png",
     texture: "data/iowa-city-streetmap.png",
     heightmapMaxElevation: 45, // lowest point around 195m, highest around 235, 45m diff
-    elevationVerticalTilt: -3,
+    elevationVerticalTilt: -6.66,
     modelHeight: 13000, // around 13km, measured on google maps
     modelWidth: 20000
   },
-  iowaAmplified1: {
+  IowaAmplified1: {
     elevation: "data/iowa-city-heightmap.png",
     riverData: "data/iowa-city-river.png",
     texture: "data/iowa-city-streetmap.png",
     heightmapMaxElevation: 45 * 5, // lowest point around 195m, highest around 235, 45m diff
-    elevationVerticalTilt: -3 * 5,
+    elevationVerticalTilt: -6.66,
     modelHeight: 13000, // around 13km, measured on google maps
     modelWidth: 20000
   },
-  iowaAmplified2: {
+  IowaAmplified2: {
     elevation: "data/iowa-city-heightmap.png",
     riverData: "data/iowa-city-river.png",
     texture: "data/iowa-city-streetmap.png",
     heightmapMaxElevation: 45 * 3, // lowest point around 195m, highest around 235, 45m diff
-    elevationVerticalTilt: -3 * 3,
+    elevationVerticalTilt: -6.66,
     modelHeight: 13000, // around 13km, measured on google maps
     modelWidth: 20000
   },
-  iowaAmplified3: {
+  IowaAmplified3: {
     elevation: "data/iowa-city-heightmap.png",
     riverData: "data/iowa-city-river.png",
     texture: "data/iowa-city-streetmap.png",
     heightmapMaxElevation: 45 * 20, // lowest point around 195m, highest around 235, 45m diff
-    elevationVerticalTilt: -3 * 20,
+    elevationVerticalTilt: -6.66,
     modelHeight: 13000, // around 13km, measured on google maps
     modelWidth: 20000
+  },
+  RiverCity: {
+    elevation: "data/model2_heightmap_hi.png",
+    riverData: "data/model2_map_riverdata.png",
+    texture: "data/model2_map_topo.png",
+    heightmapMaxElevation: 45, // lowest point around 195m, highest around 235, 45m diff
+    elevationVerticalTilt: -6.66,
+    modelHeight: 8000, // around 8km, looking at street map, this area is much smaler than Iowa preset
+    modelWidth: 8000
   }
 };
 
