14eeab4665c298a864e09bb8f1d79c04f4a0eb80
pjanik <janikpiotrek@gmail.com>
2020-07-08 18:58:52 +0200

    Add new test presets
    [#173471279]

--- src/presets.ts
+++ src/presets.ts
@@ -21,6 +21,35 @@ const presets: {[key: string]: Partial<ISimulationConfig>} = {
     maxElevation: 25, // m
     modelHeight: 800, // m
     modelWidth: 800 // m
+  },
+  slope: {
+    elevation: [
+      [3, 4, 5, 4, 3],
+      [2, 3, 6, 3, 2],
+      [1, 2, 3, 2, 1],
+      [0, 1, 2, 1, 0],
+      [0, 0, 0, 0, 0]
+    ],
+    waterDepth: [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ],
+    minElevation: 0, // m
+    maxElevation: 250, // m
+    modelHeight: 10, // m
+    modelWidth: 10 // m
+  },
+  waterfall: {
+    elevation: "data/waterfall_elevation.png",
+    waterDepth: "data/waterfall_water.png",
+    waterHeightmapMaxDepth: 20,
+    minElevation: 0, // m
+    maxElevation: 30, // m
+    modelHeight: 100, // m
+    modelWidth: 100 // m
   }
 };
 
