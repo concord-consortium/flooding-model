dc6d6bfc9aa13cd622668c2b3f44d13926268842
pjanik <janikpiotrek@gmail.com>
2020-08-04 21:38:48 +0200

    Cleanup cross-section-tab
    [#173990163]

--- src/components/cross-section-tab.tsx
+++ src/components/cross-section-tab.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import { interpolate } from "polymorph-js";
-// import { interpolatePath } from "d3-interpolate-path";
 import Marker1 from "../assets/marker1.svg";
 import Marker2 from "../assets/marker2.svg";
 import Marker3 from "../assets/marker3.svg";
@@ -13,7 +12,7 @@ import { useStores } from "../use-stores";
 import { RiverStage } from "../models/simulation";
 
 interface IProps {
-  gauge: number;
+  gauge: 1 | 2 | 3;
 }
 
 const Icon: {[key: number]: React.JSXElementConstructor<any>} = {
@@ -28,6 +27,7 @@ const CrossSectionBackground: {[key: number]: React.JSXElementConstructor<any>} 
   3: CSView3
 };
 
+// Semi-transparent water layer.
 const waterLevel = [
   "path[data-name='water front - LOW']",
   "path[data-name='water front - MED']",
@@ -35,6 +35,7 @@ const waterLevel = [
   "path[data-name='water front - CREST']",
 ];
 
+// White water line.
 const waterWhiteLine = [
   "path[data-name='water white line - LOW']",
   "path[data-name='water white line - MED']",
@@ -42,6 +43,7 @@ const waterWhiteLine = [
   "path[data-name='water white line - CREST']",
 ];
 
+// Dashed water line.
 const waterLine = [
   "path[data-name='water line - LOW']",
   "path[data-name='water line - MED']",
@@ -51,35 +53,49 @@ const waterLine = [
 
 const imagesCount = waterLine.length;
 
+// Cross-section view is based on SVG images and only one simulation output - riverStage.
+// SVG image is imported by webpack svgr loader and directly embedded into DOM source. That way it's possible to hide
+// some of its element using CSS and make transformations using JS. CSS hides all the water level lines by default.
+// JS code below uses polymorph-js to take water line (that consists of white line and dashed line) and transparent
+// water layer and morph between various states using `riverStage` simulation output. Morphed path is rendered
+// on top of the original SVG. All the necessary selectors are defined as constants above.
 export const CrossSectionTab: React.FC<IProps> = observer(({ gauge }) => {
   const IconComp = Icon[gauge];
   const CrossSectionBgComp = CrossSectionBackground[gauge];
 
   const { simulation } = useStores();
+  // SVG output paths.
   const waterLevelRef = useRef<SVGPathElement>(null);
   const waterWhiteLineRef = useRef<SVGPathElement>(null);
   const waterLineRef = useRef<SVGPathElement>(null);
+  // SVG path interpolators used to morph one path into another.
+  const waterLineInterpolator = useRef<(offset: number) => string>();
+  const waterWhiteLineInterpolator = useRef<(offset: number) => string>();
+  const waterLevelInterpolator = useRef<(offset: number) => string>();
 
   const riverStage = simulation.riverStage;
+  // Get path between path defined by startImgIdx and startImgIdx + 1.
   const startImgIdx = Math.max(0, Math.floor(((riverStage - RiverStage.Low) / RiverStage.Crest) * imagesCount));
-  const stepSize = (RiverStage.Crest - RiverStage.Low) / (imagesCount - 1);
-  const stepProgress = (riverStage % stepSize) / stepSize;
 
+  // Keep interpolator updates separate from interpolation itself for performance reasons.
+  // Interpolators need to be updated only when path idx needs to change, while the final path is updated more often.
   useEffect(() => {
     const endImageIdx = Math.min(imagesCount - 1, startImgIdx + 1);
+    waterLineInterpolator.current = interpolate([waterLine[startImgIdx], waterLine[endImageIdx]]);
+    waterWhiteLineInterpolator.current = interpolate([waterWhiteLine[startImgIdx], waterWhiteLine[endImageIdx]]);
+    waterLevelInterpolator.current = interpolate([waterLevel[startImgIdx], waterLevel[endImageIdx]]);
+  }, [startImgIdx]);
 
-    const waterLineInterpolator = interpolate([
-      document.querySelector(waterLine[startImgIdx])?.getAttribute("d") || "",
-      document.querySelector(waterLine[endImageIdx])?.getAttribute("d") || ""
-    ]);
-    waterLineRef.current?.setAttribute("d", waterLineInterpolator(stepProgress));
+  useEffect(() => {
+    // Interpolation between LOW -> MED, MED -> HIGH, or HIGH -> CREST will take the same amount of time.
+    const stepSize = (RiverStage.Crest - RiverStage.Low) / (imagesCount - 1);
+    // Value [0, 1] that defines interpolation level between <pathName>[startImgIdx] and <pathName>[startImgIdx + 1].
+    const stepProgress = (riverStage % stepSize) / stepSize;
 
-    const waterWhiteLineInterpolator = interpolate([waterWhiteLine[startImgIdx], waterWhiteLine[endImageIdx]]);
-    waterWhiteLineRef.current?.setAttribute("d", waterWhiteLineInterpolator(stepProgress));
-
-    const waterLevelInterpolator = interpolate([waterLevel[startImgIdx], waterLevel[endImageIdx]]);
-    waterLevelRef.current?.setAttribute("d", waterLevelInterpolator(stepProgress));
-  }, [startImgIdx, stepProgress]);
+    waterLineRef.current?.setAttribute("d", waterLineInterpolator.current?.(stepProgress) || "");
+    waterWhiteLineRef.current?.setAttribute("d", waterWhiteLineInterpolator.current?.(stepProgress) || "");
+    waterLevelRef.current?.setAttribute("d", waterLevelInterpolator.current?.(stepProgress) || "");
+  }, [riverStage]);
 
   return (
     <div className={css.crossSection}>
@@ -91,7 +107,9 @@ export const CrossSectionTab: React.FC<IProps> = observer(({ gauge }) => {
           <CrossSectionBgComp />
         </div>
         <div className={css.waterLine}>
+          {/* Viewbox should match background SVG viewBox */}
           <svg viewBox="0 0 402 157">
+            {/* Styles are taken from background SVG */}
             <path ref={waterLevelRef} style={{ fill: "#50acff", opacity: 0.4 }}/>
             <path ref={waterWhiteLineRef} style={{ fill: "none", stroke: "#fff", strokeMiterlimit: 10, opacity: 0.6 }}/>
             <path ref={waterLineRef} style={{ fill: "none", stroke: "#1b96db", strokeMiterlimit: 10, strokeDasharray: "8,4" }}/>
