7dd6b9aaa1426ac7f320b1efbd5bd849adee6c4e
pjanik <janikpiotrek@gmail.com>
2020-09-11 15:23:56 +0200

    Add SnapshotManager tests
    [#173471204]

---
+++ src/models/snapshots-manager.test.ts
@@ -0,0 +1,95 @@
+import { SimulationModel } from "./simulation";
+import { SnapshotsManager } from "./snapshots-manager";
+
+const getSimpleSimulation = async () => {
+  const s = new SimulationModel({
+    elevation: [[0]],
+    riverData: null,
+    gridWidth: 1
+  });
+  await s.dataReadyPromise;
+  return s;
+};
+
+describe("SnapshotsManager", () => {
+  it("it saves snapshot each 6 hours", async () => {
+    const sim = await getSimpleSimulation();
+    const snapManager = new SnapshotsManager(sim);
+    expect(snapManager.maxDay).toEqual(0);
+    expect(snapManager.snapshots.length).toEqual(0);
+
+    while (sim.timeInHours < 5) {
+      sim.tick();
+    }
+    expect(snapManager.maxDay).toEqual(0);
+    expect(snapManager.snapshots.length).toEqual(1);
+    while (sim.timeInHours < 6) {
+      sim.tick();
+    }
+    expect(snapManager.maxDay).toEqual(0.25);
+    expect(snapManager.snapshots.length).toEqual(2);
+    while (sim.timeInHours < 24) {
+      sim.tick();
+    }
+    expect(snapManager.maxDay).toEqual(1);
+    expect(snapManager.snapshots.length).toEqual(5);
+  });
+
+  it("it restores snapshot correctly", async () => {
+    const sim = await getSimpleSimulation();
+    const snapManager = new SnapshotsManager(sim);
+    expect(snapManager.maxDay).toEqual(0);
+    expect(snapManager.snapshots.length).toEqual(0);
+
+    while (sim.timeInHours < 24) {
+      sim.tick();
+    }
+    expect(snapManager.maxDay).toEqual(1);
+    expect(snapManager.snapshots.length).toEqual(5);
+
+    const restoreSpy = jest.spyOn(sim, "restoreSnapshot");
+    const stopSpy = jest.spyOn(sim, "stop");
+    snapManager.restoreSnapshot(0.5);
+    expect(restoreSpy).toHaveBeenCalledWith(snapManager.snapshots[2].simulationSnapshot);
+    expect(stopSpy).toHaveBeenCalled();
+  });
+
+  it("it handles simulation restart", async () => {
+    const sim = await getSimpleSimulation();
+    const snapManager = new SnapshotsManager(sim);
+    expect(snapManager.maxDay).toEqual(0);
+    expect(snapManager.snapshots.length).toEqual(0);
+
+    while (sim.timeInHours < 24) {
+      sim.tick();
+    }
+    expect(snapManager.maxDay).toEqual(1);
+    expect(snapManager.snapshots.length).toEqual(5);
+
+    sim.restart();
+    expect(snapManager.maxDay).toEqual(0);
+    expect(snapManager.snapshots.length).toEqual(0);
+  });
+
+  it("it handles simulation start", async () => {
+    const sim = await getSimpleSimulation();
+    const snapManager = new SnapshotsManager(sim);
+    expect(snapManager.maxDay).toEqual(0);
+    expect(snapManager.snapshots.length).toEqual(0);
+
+    while (sim.timeInHours < 24) {
+      sim.tick();
+    }
+    expect(snapManager.maxDay).toEqual(1);
+    expect(snapManager.snapshots.length).toEqual(5);
+
+    snapManager.restoreSnapshot(0.5);
+    expect(snapManager.maxDay).toEqual(1);
+    expect(snapManager.snapshots.length).toEqual(5);
+
+    sim.start();
+    // Clear all the data that is after current point in time once simulation is started again.
+    expect(snapManager.maxDay).toEqual(0.5);
+    expect(snapManager.snapshots.length).toEqual(3);
+  });
+});
