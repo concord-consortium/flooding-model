eb4f043c6293af75bb2315b1fc85ed6ff0959318
pjanik <janikpiotrek@gmail.com>
2021-07-26 17:45:11 +0200

    Add top bar
    [#178757999]

---
+++ src/geohazard-components/top-bar/dialog.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import MuiDialog from "@material-ui/core/Dialog";
+import CloseIcon from "@material-ui/icons/Close";
+import * as css from "./dialog.scss";
+
+interface IProps {
+  onClose: () => void;
+  open: boolean;
+  title?: string;
+}
+
+export const Dialog: React.FC<IProps> = ({ onClose, open, title, children }) => (
+  <MuiDialog
+    onClose={onClose}
+    open={open}
+    maxWidth="lg"
+  >
+    <div className={css.dialogBody}>
+      <div className={css.title}>{ title }</div>
+      <CloseIcon className={css.closeButton} onClick={onClose} />
+      <div className={css.content}>{ children }</div>
+    </div>
+  </MuiDialog>
+);
